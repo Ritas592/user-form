0f09123d91de67f74facf7db9fa08f34853a1510
Rita Sciuvra <rita.sciuvra92@gmail.com>
2022-07-13 09:48:23 +0200

    feat: add Home page

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import ButtonLink from '../../component/ButtonLink';
+
+const Home = () : JSX.Element => (
+  <>
+    <Container className="box my-5">
+      <Row>
+        <Col>
+          <h2 style={{ textAlign: 'center', marginLeft: '10px' }}>
+            Welcome
+          </h2>
+        </Col>
+      </Row>
+      <Row>
+        <ButtonLink
+          link="/signup"
+          title="Signup"
+          className="my-3 w-50"
+        />
+      </Row>
+      <Row>
+        <ButtonLink
+          link="/search"
+          title="Search"
+          className="my-3 w-50"
+        />
+      </Row>
+    </Container>
+  </>
+);
+
+export default Home;
