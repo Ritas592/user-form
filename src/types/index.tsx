62dc9b8dfcc0b2c961a20a89cdfd252c06888929
Rita Sciuvra <rita.sciuvra92@gmail.com>
2022-07-13 09:50:43 +0200

    feat: add types

---
+++ src/types/index.tsx
@@ -0,0 +1,14 @@
+export type UsersData = {
+  name: string
+  email: string
+  gender: string
+  status: string
+};
+
+export type UsersSearch = {
+  id: string
+  name: string
+  email: string
+  gender: string
+  status: string
+};
