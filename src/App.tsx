1f15cbdeea273798f0ad15d8f01de83e2714f369
Rita Sciuvra <rita.sciuvra92@gmail.com>
2022-07-13 09:47:58 +0200

    feat: add route

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Container, Row, Col } from 'react-bootstrap';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Home from './pages/Home';
+import Search from './pages/Search';
+import Success from './pages/Success';
+import Signup from './pages/Signup';
+
+import './App.scss';
+
+const App = (): JSX.Element => (
+  <Container className="App">
+    <Row className="justify-content-center">
+      <Col xs={12} md={6}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/success" element={<Success />} />
+            <Route path="/search" element={<Search />} />
+          </Routes>
+        </BrowserRouter>
+      </Col>
+    </Row>
+  </Container>
+);
 
 export default App;
